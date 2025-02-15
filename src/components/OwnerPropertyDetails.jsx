72a4625c7fd3e39336be403222331825033b76e9
Julie Nalband <julia_157nalband@outlook.com>
2025-02-15 13:13:37 +0100

    ändrade till -1

--- src/components/OwnerPropertyDetails.jsx
+++ src/components/OwnerPropertyDetails.jsx
@@ -139,7 +139,7 @@ const OwnerPropertyDetails = () => {
                                    onClick={async () => {
                                     await handleDelete(selectedProperty?._id); 
                                     setShowModal(false);
-                                    navigate("my-properties")
+                                    navigate(-1)
                                 }}
                                     className="bg-red-600 text-white px-4 py-2 rounded-lg hover:bg-red-700 transition-all"
                                 >
